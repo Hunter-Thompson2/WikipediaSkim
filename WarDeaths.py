dfd660ffe4cf4123d4a7c29ebdf945008695587b
Hunter-Thompson2 <hunterthompson2@my.unt.edu>
2021-06-20 20:20:32 -0500

    finishing
    fixed some visual bug in the graph and added final comments. also split graph code into a function

--- WarDeaths.py
+++ WarDeaths.py
@@ -1,4 +1,5 @@
-
+# Hunter Thompson
+# 6/20/2021
 import requests
 import re
 from bs4 import BeautifulSoup
@@ -33,23 +34,25 @@ def createPairs(wars):
     pairs = []          
     #loop through each table element of wars
     for war in wars:
-        #split based on new line
+        #split based on new line to break each table element into its individual lines 
         lines = war.text.split("\n")
-        #get deaths, there are sometimes two numbers based on high and low estimates
-        if len(lines[3].split("–")) > 1:
+        #get deaths, there are sometimes two numbers based on high and low estimates and theyre seperated by dashes
+        if len(lines[3].split("–")) > 1:    #if this font of dash is used
             deathsFull = lines[3].split("–")
-        elif len(lines[3].split("-")) > 1:
+        elif len(lines[3].split("-")) > 1:  #if this font of dash is used
             deathsFull = lines[3].split("-")
         else:
-            deathsFull = lines[3].split("–")
+            deathsFull = lines[3].split("–")    #This is usually when there isnt a dash but i split it anyways
         #if theres two, choose the high estimate
-        if len(deathsFull) > 1:
-            deaths = deathsFull[1]
+        if len(deathsFull) > 1:     #there are two numbers
+            deaths = deathsFull[1]  #choose second (the highest)
         else:
             deaths = deathsFull[0]
         #take the starting year
         year = lines[5].split("–")[0]
+        #use regex to find digits. Some are in the format of xxxx+ or have extra citation links that need to be taken out
         deathsDigit = re.findall(r'\d+', deaths)
+        #some elements are even numbers and can fudge it
         if len(deathsDigit) > 0 and len(yearDigit) > 0:
             deathsDigit[0] = int(deathsDigit[0]) * pow(1000, len(re.findall(",", deaths)))
         yearDigit = re.findall(r'\d+', year)
@@ -62,55 +65,42 @@ def createPairs(wars):
 
 #split into sets by 50 year periods
 def aggregateByPeriod(deathsAndYear):
-    
+    #periodSize is the number of 50 year periods being accounted for
     periodSize = 41  #2050 AD/50 = 41 periods
+    #creates a list of size 41 with each starting with a count of 0 deaths
     periodTotals = [0] * periodSize
+
     for pair in deathsAndYear:
+        #takes the element containing the year for this war
         year = int(pair[1])
+        #finds the index by dividing years by 50 because the list maps to 50 year periods
         index = int(year / 50)
+        #takes the element containing the death count for this war
         deaths = int(pair[0])
+        #adds it to teh total for this 50 year period
         periodTotals[index] = deaths + periodTotals[index]
-
-
-    for i in range(41):
-        period = str(i*50)
-        print(str(periodTotals[i]) + " in " + period + "\n")
     
     return periodTotals
-#aggregate to 1 total
-def finalizeData(periodTotals):
-    yearAndTotal = []
-    years = []
-    for i in range(len(periodTotals)):
-        years.append(i*50)
-        pair = (i*50, periodTotals[i])
-        yearAndTotal.append(pair)
-    #index = np.arange(len(yearAndTotal))
-    #plt.bar(index, periodTotals) #add color=''
-    #plt.xticks(index, years, rotation='vertical')
-    #plt.tight_layout()
-    #plt.show()
-    #ax = plt.subplots()
-    #ax.bar(years, periodTotals, .35)
-    #ax.set_ylabel('Deaths')
-    #ax.set_title('Deaths by War')
-    
-    return years, periodTotals
+
 
 #create histogram
+def createHistogram(periodTotals):
+    #converts periodTotals to an integer
+    periodTotals = [int(i) for i in periodTotals]
+    #creates a list of years to be mapped to the totals
+    years = [int(i*50+25) for i in range(41)] #+25 to make the table visually fit the period because it was centering the bars
+    fig, ax = plt.subplots()
+    ax.bar(years, periodTotals, len(years))
+    ax.set_ylabel('Deaths')
+    ax.set_title('Deaths by War')
+    ax.legend()
+    plt.show()
+
 
 URL = "https://en.wikipedia.org/wiki/List_of_wars_by_death_toll"
 
 wars = scrape(URL)
 deathsAndYear = createPairs(wars)
-years, periodTotals = finalizeData(aggregateByPeriod(deathsAndYear))
-periodTotals = [int(i) for i in periodTotals]
-print(periodTotals)
-fig, ax = plt.subplots()
-ax.bar(years, periodTotals, len(years))
-ax.set_ylabel('Deaths')
-ax.set_title('Deaths by War')
-ax.legend()
-plt.show()
-#for war in deathsAndYear:
-    #print(war[0] + " " + war[1])
+periodTotals = aggregateByPeriod(deathsAndYear)
+createHistogram(periodTotals)
+
