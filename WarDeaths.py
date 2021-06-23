4c058c8f3b08f64d02b90c0b1766fbc795cf1d66
Hunter-Thompson2 <hunterthompson2@my.unt.edu>
2021-06-22 20:34:24 -0500

    fixed constants and comments
    constants are now controlled by global variables

--- WarDeaths.py
+++ WarDeaths.py
@@ -1,11 +1,23 @@
 # Hunter Thompson
 # 6/20/2021
+#This program scrapes the wikipedia page titled "List of wars by death toll"
+#and sums up the number of deaths by 50 year period of history. This is then
+#used as input for a matplot bar graph. Originally I wanted to use the wikipedia
+#API because I saw it in a reddit post but the API didnt like returning media/tables.
+#As most of the data I wanted for this was in tables, I gave up on the API but 
+#continued using beautiful soup to scrape it.
+
 import requests
 import re
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
 import numpy as np
 
+PERIOD_SIZE = 50
+NUM_PERIODS = 41
+VISUAL_OFFSET = 25
+
+#uses beautiful soup to get the table elements into a list of wars
 def scrape(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, features="lxml")
@@ -21,6 +33,8 @@ def scrape(url):
 
     return wars
 
+#creates a pair of (deaths, year) for each war.
+#data like the name and the actors is left out
 def createPairs(wars):
     #format of table element: 
     #0
@@ -63,10 +77,10 @@ def createPairs(wars):
             pairs.append(pair)
     return pairs
 
-#split into sets by 50 year periods
+#creates a total for each 50 year period using the list of pairs created before
 def aggregateByPeriod(deathsAndYear):
     #periodSize is the number of 50 year periods being accounted for
-    periodSize = 41  #2050 AD/50 = 41 periods
+    periodSize = NUM_PERIODS  #2050 AD/50 = 41 periods
     #creates a list of size 41 with each starting with a count of 0 deaths
     periodTotals = [0] * periodSize
 
@@ -74,7 +88,7 @@ def aggregateByPeriod(deathsAndYear):
         #takes the element containing the year for this war
         year = int(pair[1])
         #finds the index by dividing years by 50 because the list maps to 50 year periods
-        index = int(year / 50)
+        index = int(year / PERIOD_SIZE)
         #takes the element containing the death count for this war
         deaths = int(pair[0])
         #adds it to teh total for this 50 year period
@@ -83,12 +97,12 @@ def aggregateByPeriod(deathsAndYear):
     return periodTotals
 
 
-#create histogram
+#creates histogram using matplot with the input being the total for each period and the years
 def createHistogram(periodTotals):
     #converts periodTotals to an integer
     periodTotals = [int(i) for i in periodTotals]
     #creates a list of years to be mapped to the totals
-    years = [int(i*50+25) for i in range(41)] #+25 to make the table visually fit the period because it was centering the bars
+    years = [int(i*PERIOD_SIZE+VISUAL_OFFSET) for i in range(NUM_PERIODS)] #+25 to make the table visually fit the period because it was centering the bars
     fig, ax = plt.subplots()
     ax.bar(years, periodTotals, len(years))
     ax.set_ylabel('Deaths')
